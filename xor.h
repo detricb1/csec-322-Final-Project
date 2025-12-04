d3e9e38640f8cab4e29593438230f883aa34e768
detricb1 <detricbrown@go.rmc.edu>
2025-12-04 13:48:33 -0500

    Update xor.h

--- xor.h
+++ xor.h
@@ -1,10 +1,13 @@
-// xor.h
-
-#ifndef XOR_H
-#define XOR_H
+#ifndef _XOR_H
+#define _XOR_H
 
 #include <stddef.h>
 
+/*
+ * Encrypts/Decrypts the buffer in place using the key.
+ * Running this once Encrypts.
+ * Running it again on the same buffer Decrypts.
+ */
 void xor_buffer(char *buf, size_t len, unsigned long long key);
 
 #endif
